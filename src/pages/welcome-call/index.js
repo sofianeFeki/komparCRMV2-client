a0cacf5d932b619702264b3a6d8d8a36469125ec
sofianeFeki <backofficekompar@gmail.com>
2023-04-27 23:08:51 +0200

    Update index.js

--- src/pages/welcome-call/index.js
+++ src/pages/welcome-call/index.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { DataTable } from '../../components/dataGrid';
 import { MainContainer } from '../../style/mainContainer';
-import { adminColumns } from '../../components/dataGrid/columns';
+import { wcCulumns } from '../../components/dataGrid/columns';
+import { getWcRows } from '../../functions/contract';
 
 const rows = [];
 
 const WelcomeCall = () => {
-  const { drawer } = useSelector((state) => ({ ...state }));
+
+  const { drawer, quickFilter } = useSelector((state) => ({ ...state }));
+
+  const [rows, setRows] = useState([])
+  const [loading, setLoading] = useState(false);
+
+
+  const loadContract = () => {
+    setLoading(true)
+    getWcRows(quickFilter.text).then((c) => {
+      setRows(c.data);
+      console.log(c.data)
+      setLoading(false)
+
+       });
+  };
+
+
+  
+  useEffect(() => {
+    loadContract();
+    console.log(quickFilter.text)
+   
+  }, [quickFilter]);
+
   return (
     <MainContainer open={drawer}>
-      <DataTable rows={rows} columns={adminColumns} />
+      <DataTable rows={rows} columns={wcCulumns}  loading={loading} setLoadng={setLoading} 
+ />
     </MainContainer>
   );
 };

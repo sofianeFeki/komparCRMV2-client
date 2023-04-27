2268ce71637740057ad0cdb9d3448c89c4258a5b
sofianeFeki <backofficekompar@gmail.com>
2023-04-27 23:17:23 +0200

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,14 +3,12 @@ import { drawerReducer } from './drawerReducer';
 import { quickFilterReducer } from './quickFilterReducer';
 import { userReducer } from './userReducer';
 import { filtersReducer } from './filtersReducer';
-import { paginationReducer } from './paginationReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
   drawer: drawerReducer,
   quickFilter: quickFilterReducer,
   filters : filtersReducer,
-  pagination: paginationReducer,
 
 });
 

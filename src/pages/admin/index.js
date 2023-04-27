66eeb950be0147135ade014c5bf285b10de0b39d
sofianeFeki <backofficekompar@gmail.com>
2023-04-27 23:07:04 +0200

    Update index.js

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -3,14 +3,18 @@ import { useSelector } from 'react-redux';
 import { DataTable } from '../../components/dataGrid';
 import { MainContainer } from '../../style/mainContainer';
 import { adminColumns } from '../../components/dataGrid/columns';
-import { getAdminRows } from '../../functions/contract';
+import { getAdminRows, getFilters } from '../../functions/contract';
+import DraggableDialog from '../../components/dialog';
 
 
 
 const Admin = () => {
-  const { drawer } = useSelector((state) => ({ ...state }));
-  const [rows, setRows] = useState([]);
+  const { drawer, quickFilter   } = useSelector((state) => ({ ...state }));
+  const serverData = useSelector(state => state.filters.serverData);
 
+  const [rows, setRows] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [totalRowCount, setTotalRowCount] = useState(0);
 
 
   const [paginationModel, setPaginationModel] = useState({
@@ -19,24 +23,36 @@ const Admin = () => {
   });
 
   const [sortOptions, setSortOptions] = useState([ { field: 'date_de_la_signature', sort: 'desc' } ]);
-  const [loading, setLoading] = useState(false);
-  const [totalRowCount, setTotalRowCount] = useState(0);
-
+ 
+  const loadContract = () => {
+    setLoading(true);
+  
+    if (serverData && serverData.data !== null) { // check if filters exist and are not empty
+  
+        const { data, total } = serverData;
+        setRows(data);
+        setTotalRowCount(total);
+        setLoading(false);
+    
+     
+    } else {
+      getAdminRows(paginationModel, sortOptions, quickFilter).then((response) => {
+        const { data, total } = response.data;
+        setRows(data);
+        setTotalRowCount(total);
+        setLoading(false);
+      });
+    }
+  };
+  
   
 
 
-const loadContract = () => {
-  getAdminRows(paginationModel,sortOptions).then((c) => {
-    const { data, total } = c.data;
-    setRows(data);
-    setTotalRowCount(total);
-     });
 
-};
-
-useEffect(() => {
-  loadContract();
-}, [paginationModel, sortOptions]);
+  useEffect(() => {
+    loadContract();
+    console.log('--------> redux data',quickFilter );
+  }, [serverData, paginationModel, sortOptions, quickFilter ]);
 
   return (
     <MainContainer open={drawer}>

3ead027c896576acfa4b78aa58f390fad0ca7795
sofianeFeki <backofficekompar@gmail.com>
2023-04-27 23:08:26 +0200

    Update index.js

--- src/pages/sav/index.js
+++ src/pages/sav/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { DataTable } from '../../components/dataGrid';
 import { MainContainer } from '../../style/mainContainer';
-import { adminColumns } from '../../components/dataGrid/columns';
+import { adminColumns, savCulumns } from '../../components/dataGrid/columns';
+import { getSavRows } from '../../functions/contract';
 
-const rows = [{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 }];
 
 const Sav = () => {
-  const { drawer } = useSelector((state) => ({ ...state }));
+  const { drawer, quickFilter } = useSelector((state) => ({ ...state }));
+
+  const [rows, setRows] = useState([])
+  const [loading, setLoading] = useState(false);
+
+
+  const loadContract = () => {
+    setLoading(true)
+    getSavRows(quickFilter.text).then((c) => {
+      setRows(c.data);
+      console.log(c.data)
+      setLoading(false)
+       });
+  
+  };
+
+
+  
+  useEffect(() => {
+    loadContract();
+   
+  }, [quickFilter]);
 
   return (
     <MainContainer open={drawer}>
-      <DataTable rows={rows} columns={adminColumns} />
+      <DataTable rows={rows} columns={savCulumns} loading={loading} setLoadng={setLoading} />
     </MainContainer>
   );
 };

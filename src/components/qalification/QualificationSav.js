c15c0a02ebff5bbc682e120cdbb30d877be98f51
sofianeFeki <backofficekompar@gmail.com>
2023-04-27 23:02:22 +0200

    Update QualificationSav.js

--- src/components/qalification/QualificationSav.js
+++ src/components/qalification/QualificationSav.js
@@ -2,15 +2,26 @@ import React, { useEffect, useState } from "react";
 import DraggableDialog from "../dialog";
 import {  FormControl, InputLabel, MenuItem, Select, Stack, TextField } from "@mui/material";
 import { SupervisorAccount } from "@mui/icons-material";
+import { updateContractSav } from "../../functions/contract";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
-const initialQualification = ['Aucun(e)', 'validé', 'annulation', 'A relancer'];
+const initialQualification = ['aucun(e)', 'validé', 'annulation', 'A relancer'];
 
 
-const QualificationSav = () => {
+const QualificationSav = ({data}) => {
+
+
+  const {slug, energie} = useParams()
+  const {user} = useSelector((state) => ({...state}))
+  const [open, setOpen] = useState(false);
+   
+
 
   const [qualificationsSav , setQualificaionsSav] = useState({ 
-    comment : "",
-    qualification : '',
+      comment :  data.comment ?? '',
+      qualification : data.qualification ?? '',
   })
 
 
@@ -30,15 +41,54 @@ const QualificationSav = () => {
   };
 
   useEffect(() => {
-      
-    console.log(qualificationsSav)
-  }, [qualificationsSav]);
+    setQualificaionsSav(prevState => ({
+      ...prevState,
+      comment: data.comment ?? '',
+      qualification: data.qualification ?? '',
+    }));
+  }, [data]);
+
+  const handleApply = (e) => {
+    e.preventDefault();
+    toast
+    .promise(updateContractSav(slug,energie, qualificationsSav, user.token), {
+      pending: {
+        render() {
+          return 'Updating Contract...';
+        },
+        icon: '🔄',
+        // You can also set the autoClose option to false to keep the toast open
+        // while the Promise is pending.
+      },
+      success: {
+        render() {
+          return 'Contract Updated Successfully!';
+        },
+        // other options
+        icon: '👍',
+      },
+      error: {
+        render({ data }) {
+          // When the Promise rejects, data will contain the error
+          return `Error: ${data.message}`;
+        },
+        // other options
+        icon: '❌',
+      },
+    })
+    .then((res) => {
+     setOpen(false)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
   const content =   
   <Stack spacing={2} width={400} mt={1}>
    <TextField
     fullWidth
-     label="Commentaire"
+    label="Commentaire"
     id="Commentaire"
      multiline
      onChange={handleCommentChange}
@@ -71,6 +121,9 @@ return (
     buttonText = "SAV"
     title="SAV"
     text={content}
+    handleApply={handleApply}
+    setOpen={setOpen}
+    open={open}
   />
 )
 }

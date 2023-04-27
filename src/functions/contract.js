d8bdae061fa0aafc334554bcc83a176ad6aed84e
sofianeFeki <backofficekompar@gmail.com>
2023-04-27 23:04:40 +0200

    Update contract.js

--- src/functions/contract.js
+++ src/functions/contract.js
@@ -12,25 +12,80 @@ export const createContract = async (contract, authtoken) =>
   export const getContract = async (clientRef, energie) =>
   await axios.get(`${API_BASE_URL}/contract/${clientRef}/${energie}`);
 
-  export const getAdminRows = async (paginationModel, sortOptions) =>
+  export const getAdminRows = async (paginationModel, sortOptions, quickFilter) =>
   await axios.post(`${API_BASE_URL}/admin-contracts/`,  {
     paginationModel,
     sortOptions,
+    quickFilter
   });
 
-  export const getQtéRows = async (paginationModel, sortOptions) =>
+  export const getQtéRows = async (paginationModel, sortOptions, quickFilter) =>
   await axios.post(`${API_BASE_URL}/qty-contracts/`,  {
     paginationModel,
     sortOptions,
+    quickFilter
   });
 
+  export const getSavRows = async (quickFilter) =>
+  await axios.post(`${API_BASE_URL}/sav-contracts/`,  {
+   
+    quickFilter
+  });
 
-  export const getFilters = async (serverFilters) =>
+  export const getWcRows = async (quickFilter) =>
+  await axios.post(`${API_BASE_URL}/wc-contracts/`,  {
+   
+    quickFilter
+  });
+  //       ------------------ CONTRACT FILTERS ------------------
+
+  export const getFilters = async (serverFilters,  paginationModel, sortOptions) =>
   await axios.post(`${API_BASE_URL}/Filters/`,  {
-    serverFilters
+    serverFilters,
+    paginationModel,
+   sortOptions
   });
+
+  export const getquickFilters = async (quickFilter) =>
+  await axios.post(`${API_BASE_URL}/quickFilters/`,  {
+    quickFilter
+  });
+
+
+    //       ------------------ CONTRACT RESERVATION ------------------
+
 
   export const getReservation = async (id, user) =>
   await axios.post(`${API_BASE_URL}/${id}/reserve`,  {
     user
+  });
+
+
+//       ------------------ CONTRACT UPDATE ------------------
+
+
+  export const updateContractSav = async (slug, energie,qualificationsSav, authtoken) =>
+  await axios.put(`${API_BASE_URL}/contract/update/sav/${slug}/${energie}`, qualificationsSav, {
+    headers: {
+      authtoken,
+    },
+  });
+export const updateContractWc = async (slug, energie, qualificationsWc, authtoken) =>
+  await axios.put(`${API_BASE_URL}/contract/update/wc/${slug}/${energie}`, qualificationsWc, {
+    headers: {
+      authtoken,
+    },
+  });
+
+  export const updateContractQte = async (slug, energie,qualificationsQté, authtoken) =>
+  await axios.put(`${API_BASE_URL}/contract/update/quality/${slug}/${energie}`, qualificationsQté, {
+    headers: {
+      authtoken,
+    },
+  });
+
+
+  export const getContractsExport = async (filters) =>
+  await axios.post(`${API_BASE_URL}/contracts/export`, {
+    filters,
   });

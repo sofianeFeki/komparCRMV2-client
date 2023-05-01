a2ce51f7128449e16631f120c1542ca5025abece
sofianeFeki <backofficekompar@gmail.com>
2023-05-01 21:56:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useEffect } from 'react';
+import React, { lazy, Suspense, useEffect, useMemo, useState } from 'react';
 import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import CrmAppbar from './components/appbar';
 import Admin from './pages/admin';
@@ -11,40 +11,75 @@ import ContractDetails from './pages/admin/contractDetails';
 import UserProvider from './context';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Button } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useSelector } from 'react-redux';
+import RequireAuth from './protectedRoutes/RequireAuth';
 
 const Login = lazy(() => import('./pages/login'));
 
 function App() {
 
-  const {user} = useSelector((state) => ({...state}))
-  const history = useNavigate()
+  const darkMode = useSelector((state) => state.darkMode.darkMode);
 
-  useEffect(() => {
-    if (!user) {
-      history('/login');
-    } 
-  }, [history]);
+
+
+
+
+
+  //const [dark, setDark] = useState(false);
+  
+  const darkTheme = useMemo(
+   () =>
+     createTheme({
+       palette: {
+         mode: darkMode ? 'dark' : 'light',
+       },
+     }),
+   [darkMode]
+ );
 
   return (
     
     <Suspense>
-      <CrmAppbar />
+        <ThemeProvider theme={darkTheme}>
+
+      <CrmAppbar  dark={darkMode} />
       <UserProvider>
       <ToastContainer />
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route
+              element={<RequireAuth allowedRoles={['admin', 'sav']} />}
+            >
         <Route path="/sav" element={<Sav />} />
+        </Route>
+        <Route
+              element={<RequireAuth allowedRoles={['admin', 'quality']} />}
+            >
         <Route path="/quality" element={<Quality />} />
+        </Route>
+        <Route
+              element={<RequireAuth allowedRoles={['admin', 'wc']} />}
+            >
         <Route path="/welcome-call" element={<WelcomeCall />} />
+        </Route>
+        <Route element={<RequireAuth allowedRoles={['admin']} />}>
+        <Route path="/admin" element={<Admin />} />
         <Route path="/admin/contract-create" element={<ContractCreate />} />
         <Route path="/admin/users" element={<Users />} />
+        </Route>
+        <Route
+              element={
+                <RequireAuth
+                  allowedRoles={['admin', 'quality', 'wc', 'sav', 'backOffice']}
+                />
+              }
+            >
         <Route path='contract-details/:slug/:energie' element={<ContractDetails/>} />
+        </Route>
       </Routes>
       </UserProvider>
-
+      </ThemeProvider>
     </Suspense>
   );
 }

f9443028b12bd4d56012ba8ee8576952eee1f6bc
sofianeFeki <backofficekompar@gmail.com>
2023-05-01 21:53:27 +0200

    Update filtersReducer.js

--- src/reducers/filtersReducer.js
+++ src/reducers/filtersReducer.js
@@ -1,11 +1,35 @@
-export const filtersReducer = (state = { serverData: null }, action) => {
+export const filtersReducer = (state = { serverData: null, serverFilters: {
+  partenaire: '',
+  qualificationQté: '',
+  qualificationWc: '',
+  fournisseur: '',
+  date : [
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: 'selection'
+    }
+  ],
+}}, action) => {
   switch (action.type) {
     case 'SET_FILTERS':
       return { ...state, ...action.payload };
     case 'SET_SERVER_DATA':
       return { ...state, serverData: action.payload };
     case 'RESET_FILTERS':
-      return { serverData: null };
+      return {  serverFilters: {
+        partenaire: '',
+        qualificationQté: '',
+        qualificationWc: '',
+        fournisseur: '',
+        date : [
+          {
+            startDate: new Date(),
+            endDate: null,
+            key: 'selection'
+          }
+        ],
+      }};
     default:
       return state;
   }

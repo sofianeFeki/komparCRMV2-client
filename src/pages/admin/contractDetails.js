caee47c477d8c23f81583b12f32e7b5a838772b0
sofianeFeki <backofficekompar@gmail.com>
2023-05-01 21:49:32 +0200

    Update contractDetails.js

--- src/pages/admin/contractDetails.js
+++ src/pages/admin/contractDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { MainContainer } from "../../style/mainContainer";
 import { useSelector } from "react-redux";
-import { Box, Button, Chip, Divider, List, ListItem, ListItemText, ListSubheader, Paper, Stack, Typography } from "@mui/material";
+import { Box, Button, Chip, Divider, List, ListItem, ListItemText, ListSubheader, Paper, Skeleton, Stack, Typography } from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2';
 import QualificationWc from "../../components/qalification/QualificationWc";
 import QualificationQualité from "../../components/qalification/QualificationQualité";
@@ -16,11 +16,16 @@ import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
 
 const ContractDetails = () => {
 
-    const {drawer  } = useSelector((state) => ({...state}))
+    const {drawer, user  } = useSelector((state) => ({...state}))
+    const darkMode = useSelector((state) => state.darkMode.darkMode);
+    const navigate = useNavigate();
+
+
     const [data, setData] = useState([]);
     
     const {quality, sav, wc} = data
 
+    const [loading, setLoading] = useState(false);
 
     const [otherContractLink, setOtherContractLink] = useState("");
     const history = useNavigate()
@@ -28,10 +33,12 @@ const ContractDetails = () => {
     const { slug, energie } = useParams();
  
     const loadContract = () => {
+      setLoading(true)
       getContract(slug, energie).then((c) => {
         const {contract , otherContractLink} = c.data;
         setData(contract);
         setOtherContractLink(otherContractLink)
+        setLoading(false)
          });
     };
     useEffect(() => {
@@ -44,9 +51,23 @@ const ContractDetails = () => {
       history(otherContractLink)
     };
 
+    const handleBackClick = () => {
+      if (user.role === 'admin') {
+        navigate('/admin');
+      }
+      if (user.role === 'sav') {
+        navigate('/sav');
+      }
+      if (user.role === 'quality') {
+        navigate('/quality');
+      }
+      if (user.role === 'wc') {
+        navigate('/welcome-call');
+      }
+    };
 
     return(
-     <MainContainer open={drawer} sx={{  backgroundColor : grey[100] , height:"100vh"}}>
+     <MainContainer open={drawer} sx={{  backgroundColor : darkMode ? grey[800]  : grey[100] , height:"100vh"}}>
         <Box sx={{ m: 2}}>
             <Box sx={{display : 'flex' , justifyContent:'space-between'}}>
             <Stack direction="row" spacing={1}>
@@ -67,7 +88,7 @@ const ContractDetails = () => {
         {quality ? <QualificationQualité data={quality} /> : null}
         {wc ? <QualificationWc data={wc} /> : null}
         {sav ? <QualificationSav data={sav} /> : null}
-  <Button variant="outlined" component={Link} to='/admin' size="small" sx={{backgroundColor : 'white'}}>retour</Button>
+  <Button variant="outlined" onClick={handleBackClick} size="small" >retour</Button>
 </Stack>
 </Box>
       <Grid container spacing={2} sx={{ mt: 2 }} >
@@ -79,79 +100,80 @@ const ContractDetails = () => {
     >
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Ref contrat" />
-        <Typography >
-{data.contratRef}
-      </Typography>
+        {!loading ?  <Typography >{data.contratRef}</Typography>
+        :<Skeleton width={210} animation="wave" />
+        }
+       
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Ref client" />
-        <Typography >
-        {data.clientRef}
-      </Typography>
+        {!loading ? <Typography >{data.clientRef}</Typography>
+         :<Skeleton width={210} animation="wave" />
+        }
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Énergie" />
-        <Typography >
-        {data.energie}
-      </Typography>
+        {!loading ?<Typography >{data.energie}</Typography>
+        :<Skeleton width={80} animation="wave" />
+      }
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Point de livraison" />
-        <Typography >
-        {data.Point_de_livraison}
-      </Typography>
+        {!loading ? <Typography >{data.Point_de_livraison}</Typography>
+        :<Skeleton width={150} animation="wave" />
+      }
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Partenaire" />
-        <Typography >
-        {data.Nom_du_partenaire}
-      </Typography>
+        {!loading ?  <Typography >{data.Nom_du_partenaire}</Typography>
+         :<Skeleton width={150} animation="wave" />
+        }
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Date début" />
-        <Typography >
-        {moment(data.date_de_début).format('DD/MM/YYYY')}
-      </Typography>
+        {!loading ? <Typography >{moment(data.date_de_début).format('DD/MM/YYYY')}</Typography>
+        :<Skeleton width={130} animation="wave" />
+      }
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Date de signature" />
-        <Typography >
-        {moment(data.date_de_la_signature).format('DD/MM/YYYY')}
-      </Typography>
+        {!loading ? <Typography >{moment(data.date_de_la_signature).format('DD/MM/YYYY')}</Typography>
+         :<Skeleton width={130} animation="wave" />
+        }
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Mensualité" />
-        <Typography >
-        {data.Mensualité}
-      </Typography>
+        {!loading ? <Typography >{data.Mensualité}</Typography>
+        :<Skeleton width={40} animation="wave" />
+      }
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Statut" />
-        <Typography >
-        {data.Statut}
-      </Typography>
+      {!loading ?<Typography >{data.Statut} </Typography>
+        :<Skeleton width={80} animation="wave" />
+      }
       </ListItem>
       <Divider  />
       <ListItem sx={{fontWeight : 700}} >
         <ListItemText sx={{fontWeight : 700}} id="switch-list-label-wifi" primary="Puissance" />
-        <Typography >
-        {data.Puissance}
-      </Typography>
+        {!loading ? <Typography >{data.Puissance}</Typography>
+        :<Skeleton width={40} animation="wave" />
+      }
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Offre" />
-        <Typography >
-        {data.Offre}
-      </Typography>
+        {!loading ?  <Typography >{data.Offre}</Typography>
+        :<Skeleton width={70} animation="wave" />
+      }
       </ListItem>
     </List>
     </Paper>
@@ -164,33 +186,36 @@ const ContractDetails = () => {
     >
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Contact" />
-        <Typography >
-        {data.Civility} {data.Prénom} {data.Nom}
-      </Typography>
+        {!loading ?  <Typography >{data.Civility} {data.Prénom} {data.Nom}</Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Tel" />
-        <Typography >
-        {data.tel}
-      </Typography>
+        {!loading ?  <Typography >{data.Tél}</Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Email" />
-        <Typography >
-        {data.email}
-      </Typography>
+        {!loading ? <Typography >{data.Email}</Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
       </ListItem>
       <Divider  />
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Adresse" />
-        <Typography >
-        {data.Adresse} {data.Code_postal} {data.Commune}
-      </Typography>
+        {!loading ? <Typography >{data.Adresse} {data.Code_postal} {data.Commune}</Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
       </ListItem>      
     </List>
     </Paper>
+    
+    {user && user.role === 'admin' && (
+  <>
     <Paper elevation={3} sx={{mt:1}}>
         <List
       sx={{ width: '100%' }}
@@ -217,15 +242,15 @@ const ContractDetails = () => {
     >
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Qualification " />
-       <Typography >
-       { wc &&  wc.qualification}
-      </Typography>
+        {!loading ? <Typography >{ wc &&  wc.qualification}</Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
       </ListItem>
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Comentaire" />
-        <Typography >
-        { wc &&  wc.comment}
-      </Typography>
+        {!loading ?  <Typography >{ wc &&  wc.comment}</Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
       </ListItem>
     </List>
     </Paper>
@@ -236,21 +261,116 @@ const ContractDetails = () => {
     >
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Qualification " />
+        {!loading ? <Typography >{ sav &&  sav.qualification}</Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
+      </ListItem>
+      <ListItem >
+        <ListItemText id="switch-list-label-wifi" primary="Comentaire" />
+        {!loading ? <Typography >{ sav &&  sav.comment} </Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
+      </ListItem>
+    </List>
+    </Paper>
+  </>
+)}
+
+{user && user.role === 'quality' && (
+  <>
+     <Paper elevation={3} sx={{mt:1}}>
+        <List
+      sx={{ width: '100%' }}
+      subheader={<ListSubheader>Détail welcome call </ListSubheader>}
+    >
+      <ListItem >
+        <ListItemText id="switch-list-label-wifi" primary="Qualification " />
+        {!loading ? <Typography >{ wc &&  wc.qualification}</Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
+      </ListItem>
+      <ListItem >
+        <ListItemText id="switch-list-label-wifi" primary="Comentaire" />
+        {!loading ?  <Typography >{ wc &&  wc.comment}</Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
+      </ListItem>
+    </List>
+    </Paper>
+  </>
+)}
+
+{user && user.role === 'wc' && (
+  <>
+     <Paper elevation={3} sx={{mt:1}}>
+        <List
+      sx={{ width: '100%' }}
+      subheader={<ListSubheader>Détail qualité </ListSubheader>}
+    >
+      <ListItem >
+        <ListItemText id="switch-list-label-wifi" primary="Qualification " />
         <Typography >
-        { sav &&  sav.qualification}
+        { quality &&  quality.qualification}
       </Typography>
       </ListItem>
       <ListItem >
         <ListItemText id="switch-list-label-wifi" primary="Comentaire" />
         <Typography >
-        { sav &&  sav.comment} 
-      </Typography>
+        {quality && quality.comment && quality.comment.slice(0, 80) + (quality.comment.length > 50 ? '...' : '')}
+        </Typography>
       </ListItem>
     </List>
     </Paper>
+  </>
+)}
+
+{user && user.role === 'sav' && (
+  <>
+  <Paper elevation={3} sx={{mt:1}}>
+        <List
+      sx={{ width: '100%' }}
+      subheader={<ListSubheader>Détail qualité </ListSubheader>}
+    >
+      <ListItem >
+        <ListItemText id="switch-list-label-wifi" primary="Qualification " />
+        <Typography >
+        { quality &&  quality.qualification}
+      </Typography>
+      </ListItem>
+      <ListItem >
+        <ListItemText id="switch-list-label-wifi" primary="Comentaire" />
+        <Typography >
+        {quality && quality.comment && quality.comment.slice(0, 80) + (quality.comment.length > 50 ? '...' : '')}
+        </Typography>
+      </ListItem>
+    </List>
+    </Paper> 
+    <Paper elevation={3} sx={{mt:1}}>
+        <List
+      sx={{ width: '100%' }}
+      subheader={<ListSubheader>Détail welcome call </ListSubheader>}
+    >
+      <ListItem >
+        <ListItemText id="switch-list-label-wifi" primary="Qualification " />
+        {!loading ? <Typography >{ wc &&  wc.qualification}</Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
+      </ListItem>
+      <ListItem >
+        <ListItemText id="switch-list-label-wifi" primary="Comentaire" />
+        {!loading ?  <Typography >{ wc &&  wc.comment}</Typography>
+        :<Skeleton width={210} animation="wave" />
+      }
+      </ListItem>
+    </List>
+    </Paper>
+     </>
+)}
+   
         </Grid>
       </Grid>
       </Box>
+
         
      </MainContainer>
     )

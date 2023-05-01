b7a5df964454096c330086cc2de1ecfca770ccc8
sofianeFeki <backofficekompar@gmail.com>
2023-05-01 21:50:08 +0200

    Update users.js

--- src/pages/admin/users.js
+++ src/pages/admin/users.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { MainContainer } from '../../style/mainContainer';
 
 import { usersColumns } from '../../components/dataGrid/columns';
 import { DataTable } from '../../components/dataGrid';
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
+import { getUsers } from '../../functions/user';
+import { grey } from '@mui/material/colors';
 
 
 const Users = () => {
   const { drawer } = useSelector((state) => ({ ...state }));
+  const darkMode = useSelector((state) => state.darkMode.darkMode);
+
+
+  const [rows , setRows] = useState([])
+
+const loadUsers = () => {
+  getUsers().then((c) => {  
+    setRows(c.data);
+     });
+
+};
+
+useEffect(() => {
+  loadUsers();
+}, []);
+
 
   return (
-    <MainContainer open={drawer}>
-      <DataTable rows={rows} columns={usersColumns} />
+    <MainContainer open={drawer} sx={{backgroundColor : darkMode ? "auto"  : grey[100] }}>
+      <DataTable rows={rows} columns={usersColumns}  />
     </MainContainer>
   );
 };

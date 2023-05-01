b5fa6c37d71d5ad5ba7431708b5ae1458a35487b
sofianeFeki <backofficekompar@gmail.com>
2023-05-01 21:57:03 +0200

    Add files via upload

---
+++ src/protectedRoutes/RequireAuth.js
@@ -0,0 +1,22 @@
+import { useLocation, Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Auth from '../pages/auth/Auth';
+
+
+const RequireAuth = ({ allowedRoles }) => {
+  const { user } = useSelector((state) => ({ ...state }));
+
+  
+
+  const location = useLocation();
+
+  return allowedRoles.find((role) => user && user.role.includes(role)) ? (
+    <Outlet />
+  ) : user && user?.token ? (
+    <Navigate to="/unauthorized" state={{ from: location }} replace />
+  ) : (
+    <Auth />
+  );
+};
+
+export default RequireAuth;

3d7e28b7dd70280fb28be0a524b2b5809b7d22e8
sofianeFeki <backofficekompar@gmail.com>
2023-05-01 21:53:46 +0200

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,16 @@ import { drawerReducer } from './drawerReducer';
 import { quickFilterReducer } from './quickFilterReducer';
 import { userReducer } from './userReducer';
 import { filtersReducer } from './filtersReducer';
+import { paginationAndSortReducer  } from './paginationAndSortReducer ';
+import darkModeReducer from './darkModeReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
   drawer: drawerReducer,
   quickFilter: quickFilterReducer,
   filters : filtersReducer,
-
+  paginationAndSortReducer : paginationAndSortReducer ,
+  darkMode: darkModeReducer,
 });
 
 export default rootReducer;

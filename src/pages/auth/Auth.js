8a87a804366d375675fc9ce1fdf6285a377b6343
sofianeFeki <backofficekompar@gmail.com>
2023-05-22 17:19:42 +0200

    Update Auth.js

--- src/pages/auth/Auth.js
+++ src/pages/auth/Auth.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { Box, Button, Typography } from '@mui/material';
-//import logo from '../../images/logo/logo.png';
+import logo from '../../images/logo.png';
+import { useSelector } from 'react-redux';
 
 const Auth = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+
   return (
     <div>
       <Box
@@ -18,17 +21,28 @@ const Auth = () => {
           textAlign: 'center',
         }}
       >
-        {' '}
-        {/* <img src={logo} alt="Kompar logo" /> */}
-        <Box sx={{ m: 2 }}>
+        
+        <img src={logo} alt="Kompar logo" />
+      
+        {user && user.token 
+        ?  <Box sx={{ m: 2 }}>
+        <Typography variant="body1">
+          Welcome to KomparCRM <br /> you are alredy connected navigate to work space
+        </Typography>
+        <Button variant="contained" sx={{ mt: 2 }} component={Link} to={`${user.role}`}>
+        work space
+      </Button>
+      </Box>
+        :  <Box sx={{ m: 2 }}>
           <Typography variant="body1">
             Welcome to KomparCRM <br /> Log in with your kompar account to
             continue
           </Typography>
-        </Box>
-        <Button variant="contained" component={Link} to="/login">
+          <Button variant="contained" sx={{ mt: 2 }} component={Link} to="/login">
           Log in
         </Button>
+        </Box> }
+       
       </Box>
     </div>
   );
